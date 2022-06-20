8db43fadf579c1ef875b6cd18ac13c1c67655d76
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-20 18:58:57 +0430

    Updates in nav

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,13 @@
+import { useHistory } from 'react-router-dom';
+
 const Navbar = () => {
+    const history = useHistory();
+
     return (
         <nav className="navbar navbar-light bg-white shadow-0 py-3">
             <div className="container">
-                <h1 href="/" className="text-success fw-bold">Docs</h1>
-                <button className="btn btn-lg btn-success fw-bold">Get started</button>
+                <h1 onClick={() => history.push('/')} className="pointer text-success fw-bold">Docs</h1>
+                <button className="btn btn-lg btn-success fw-bold" onClick={() => history.push('/api')}>Get started</button>
             </div>
         </nav>
     );
