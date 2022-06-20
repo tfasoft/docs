4831148a0fdebd7110c3aec53afd1d6f4f1a6f20
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-20 18:40:39 +0430

    Create navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-light bg-white shadow-0 py-3">
+            <div className="container">
+                <h1 href="/" className="text-success fw-bold">Docs</h1>
+                <button className="btn btn-lg btn-success fw-bold">Get started</button>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
