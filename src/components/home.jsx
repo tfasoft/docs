ed84e6c3ece76757620d70481a0bac5e14f1e0c1
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-20 18:59:02 +0430

    Fix routes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,8 @@
+import { useHistory } from 'react-router-dom';
+
 const Home = () => {
+    const history = useHistory();
+
     return (
         <div className="pt-5">
             <div className="row">
@@ -9,7 +13,7 @@ const Home = () => {
                         <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quam lacus, cursus nec ante non, faucibus interdum enim. Maecenas dictum enim sit amet malesuada tempor. Curabitur at tempus risus. Mauris ultricies lorem elit, eu laoreet odio malesuada at. Suspendisse nec volutpat diam, finibus tristique purus. Nam ac dui et ligula malesuada feugiat. Sed eu sapien eu lectus aliquam mattis. Cras vitae lorem sed justo faucibus vestibulum. Sed a felis venenatis massa scelerisque efficitur at at lacus. Donec aliquam, lacus porttitor pretium placerat, enim ante ultricies neque, sit amet euismod neque mi id tellus. Donec nec gravida eros. Mauris scelerisque pretium ex a ornare. Vestibulum suscipit cursus metus sit amet pharetra. Nulla fermentum augue enim, at pretium elit mattis imperdiet. Aliquam lacus justo, laoreet ac turpis nec, aliquam maximus dolor.</h5>
                         <br/>
                         <div className="d-flex justify-content-between">
-                            <button className="btn btn-lg btn-success fw-bold">Get started</button>
+                            <button className="btn btn-lg btn-success fw-bold" onClick={() => history.push('/api')}>Get started</button>
                             <button className="btn btn-lg btn-link fw-bold text-success shadow">View on Github</button>
                         </div>
                     </div>
