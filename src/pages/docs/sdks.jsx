ae857d090dbbe4bbbc87886ab5d50d0b39a18828
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-19 20:12:14 +0430

    Adding sample

---
+++ src/pages/docs/sdks.jsx
@@ -0,0 +1,19 @@
+import {
+    Box
+} from "@mui/material";
+
+import DocsTitle from "../../components/text/docstitle";
+import DocsSubTitle from "../../components/text/docssubtitle";
+import DocsParagraph from "../../components/text/docsparagraph";
+
+const SDKsPage = () => {
+    return (
+        <Box>
+            <DocsTitle>Hello</DocsTitle>
+            <DocsSubTitle>Hello</DocsSubTitle>
+            <DocsParagraph>Hello</DocsParagraph>
+        </Box>
+    );
+}
+
+export default SDKsPage;
