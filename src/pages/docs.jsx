02cc91f26923f972e258cce527b3991f377c26c7
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-19 17:23:03 +0430

    Add drawer

--- src/pages/docs.jsx
+++ src/pages/docs.jsx
@@ -1,16 +1,107 @@
 import {
     Box,
-    Typography
+    Typography,
+    Drawer,
+    List,
+    ListItemButton,
+    ListItemText,
+    ListSubheader,
+    Toolbar,
+    Divider,
 } from "@mui/material";
+
+const drawerWidth = 260;
+
+const docsItem = [
+    {
+        title: "Docs Introduction",
+        href: "/docs",
+    },
+    {
+        title: "API",
+        href: "/docs/api",
+    },
+    {
+        title: "Status codes",
+        href: "/docs/codes",
+    }
+];
+
+const sdkItems = [
+    {
+        title: "SDKs Introduction",
+        href: "/sdks",
+    },
+    {
+        title: "NodeJs",
+        href: "/sdks/nodejs",
+    },
+    {
+        title: "Python",
+        href: "/sdks/python",
+    },
+    {
+        title: "Laravel",
+        href: "/sdks/laravel",
+    },
+    {
+        title: ".Net",
+        href: "/sdks/.net",
+    }
+];
 
 const DocsPage = () => {
     return (
-        <Box>
-            <Typography
-                color="primary"
+        <Box
+            sx={{
+                display: "flex",
+            }}
+        >
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+                }}
+                variant="permanent"
+                anchor="left"
             >
-                Docs page
-            </Typography>
+                <Toolbar />
+                <Divider />
+                <List
+                    subheader={<ListSubheader>Docs</ListSubheader>}
+                >
+                    {docsItem.map((item, index) => (
+                        <ListItemButton
+                            key={item}
+                            onClick={() => console.log(item.href)}
+                        >
+                            <ListItemText primary={item.title} />
+                        </ListItemButton>
+                    ))}
+                </List>
+                <Divider />
+                <List
+                    subheader={<ListSubheader>SDks</ListSubheader>}
+                >
+                    {sdkItems.map((item, index) => (
+                        <ListItemButton
+                            key={item}
+                            onClick={() => console.log(item.href)}
+                        >
+                            <ListItemText primary={item.title} />
+                        </ListItemButton>
+                    ))}
+                </List>
+                <Divider />
+            </Drawer>
+            <Box
+                sx={{
+                    width: `calc(100% - ${drawerWidth})`
+                }}
+            >
+                Docs are here
+            </Box>
         </Box>
     );
 }
