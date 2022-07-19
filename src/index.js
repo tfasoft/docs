130b28d2599fa9c2ebf953660e7562a3f127fb7c
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-19 15:21:51 +0430

    Remove mdb

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
-
-import 'mdb-ui-kit/css/mdb.min.css';
-import 'mdb-ui-kit/js/mdb.min.js';
-
 import './App.sass';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
