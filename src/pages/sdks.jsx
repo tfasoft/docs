149a302988c224bd4c7e7a2238a52485798a9173
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-19 15:20:52 +0430

    SDK pages

---
+++ src/pages/sdks.jsx
@@ -0,0 +1,18 @@
+import {
+    Box,
+    Typography
+} from "@mui/material";
+
+const SDKsPage = () => {
+    return (
+        <Box>
+            <Typography
+                color="primary"
+            >
+                SDKs page
+            </Typography>
+        </Box>
+    );
+}
+
+export default SDKsPage;
