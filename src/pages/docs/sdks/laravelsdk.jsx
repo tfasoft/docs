86b122f0ea467c9795e7beea6bd417f5f6e9edfb
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-20 00:16:53 +0430

    Add comings soon to laravel

--- src/pages/docs/sdks/laravelsdk.jsx
+++ src/pages/docs/sdks/laravelsdk.jsx
@@ -3,13 +3,14 @@ import {
 } from "@mui/material";
 
 import DocsTitle from "../../../components/text/docstitle";
-import DocsSubTitle from "../../../components/text/docssubtitle";
+// import DocsSubTitle from "../../../components/text/docssubtitle";
 import DocsParagraph from "../../../components/text/docsparagraph";
 
 const LaravelSDK = () => {
     return (
         <Box>
             <DocsTitle>Laravel SDK</DocsTitle>
+            <DocsParagraph>Coming soon . . .</DocsParagraph>
         </Box>
     );
 }
