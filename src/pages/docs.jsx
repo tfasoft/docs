34f10cd761ef88169d4374874059e904d089ca4f
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-19 15:21:07 +0430

    Docs page

---
+++ src/pages/docs.jsx
@@ -0,0 +1,18 @@
+import {
+    Box,
+    Typography
+} from "@mui/material";
+
+const DocsPage = () => {
+    return (
+        <Box>
+            <Typography
+                color="primary"
+            >
+                Docs page
+            </Typography>
+        </Box>
+    );
+}
+
+export default DocsPage;
