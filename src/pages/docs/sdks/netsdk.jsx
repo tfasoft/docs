73e258cbe213324eb9e8d5e1be3e1c3e94244b1d
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-20 00:17:02 +0430

    Add comings soon to .Net

--- src/pages/docs/sdks/netsdk.jsx
+++ src/pages/docs/sdks/netsdk.jsx
@@ -3,13 +3,14 @@ import {
 } from "@mui/material";
 
 import DocsTitle from "../../../components/text/docstitle";
-import DocsSubTitle from "../../../components/text/docssubtitle";
+// import DocsSubTitle from "../../../components/text/docssubtitle";
 import DocsParagraph from "../../../components/text/docsparagraph";
 
 const NetSDK = () => {
     return (
         <Box>
             <DocsTitle>.Net SDK</DocsTitle>
+            <DocsParagraph>Coming soon . . .</DocsParagraph>
         </Box>
     );
 }
