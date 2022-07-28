753b24d74b6e5b4ba3d357a184c8278854b08f3d
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-28 12:43:14 +0430

    Update code block with a br at the end

--- src/components/codeblock.jsx
+++ src/components/codeblock.jsx
@@ -2,19 +2,22 @@ import { Box } from "@mui/material";
 
 const CodeBlock = (props) => {
     return (
-        <Box
-            sx={{
-                bgcolor: "#002240",
-                borderRadius: 2,
-                color: "white",
-                fontSize: 17,
-                fontFamily: "Roboto Mono",
-                p: 3,
-            }}
-        >
-            {
-                props.children
-            }
+        <Box>
+            <Box
+                sx={{
+                    bgcolor: "#002240",
+                    borderRadius: 2,
+                    color: "white",
+                    fontSize: 17,
+                    fontFamily: "Roboto Mono",
+                    p: 3,
+                }}
+            >
+                {
+                    props.children
+                }
+            </Box>
+            <br />
         </Box>
     );
 }
