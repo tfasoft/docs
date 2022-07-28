f755638fe969a2f53ec18b625e3a16ef176bc975
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-28 12:15:56 +0430

    Add php instead of laravel

---
+++ src/pages/docs/sdks/phpsdk.jsx
@@ -0,0 +1,18 @@
+import {
+    Box
+} from "@mui/material";
+
+import DocsTitle from "../../../components/text/docstitle";
+// import DocsSubTitle from "../../../components/text/docssubtitle";
+import DocsParagraph from "../../../components/text/docsparagraph";
+
+const PhpSDK = () => {
+    return (
+        <Box>
+            <DocsTitle>Php SDK</DocsTitle>
+            <DocsParagraph>Coming soon . . .</DocsParagraph>
+        </Box>
+    );
+}
+
+export default PhpSDK;
